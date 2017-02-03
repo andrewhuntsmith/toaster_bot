126cea880908042f9724b645355683be77829700
Andy Smith <andrewhuntsmith@gmail.com>
2017-02-02 16:19:11 -0800

    Copied files from personal machine onto git project

--- irc.py
+++ irc.py
@@ -1,1 +1,34 @@
 #irc.py
+
+import socket
+import sys
+from time import sleep
+
+class IRC:
+
+	irc = socket.socket()
+	
+	def __init__(self):
+		self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.port = 6667
+		
+	def send(self, chan, msg):
+		self.irc.send("PRIVMSG " + chan + " :" + msg + "\n")
+	
+	def connect (self, server, channel, nickname, oauth):
+		print "connecting to: " + server
+		self.irc.connect((server,self.port))
+		self.irc.send("PASS " + oauth + "\r\n")
+		#self.irc.send("USER " + nickname + " " + nickname + " " + nickname + " :Toastbot is here!\r\n")
+		self.irc.send("NICK " + nickname + "\r\n")
+		self.irc.send("JOIN " + channel + "\r\n")
+		
+	def get_text(self):
+		text = self.irc.recv(2040)
+		
+		if text.find('PING') != -1:
+			print(text)
+			self.irc.send('PONG ' + text.split() [1] + '\r\n')
+			print('PONG ' + text.split()[1])
+			
+		return text
--- toaster_bot.py
+++ toaster_bot.py
@@ -1,1 +1,80 @@
 #toaster_bot.py
+
+
+from irc import *
+from time import sleep
+import os
+import random
+import thread
+import re
+from config import *
+
+state = 0		# 0 = normal, 1 = gathering votes
+votingDict = {}
+
+
+def voteTimer():
+	sleep(20)
+	state = 0
+	irc.send(CHAN,"Voting has ended!")
+	
+def addGuess(user, guess):
+	votingDict[user] = int(guess)
+	print(votingDict)
+	
+def clearDict():
+	votingDict.clear()
+
+def tally(result):
+	print("Tallying results")
+	print(votingDict)
+	closestUser = ""
+	closestGuess = 999999
+	for user in votingDict:
+		guess = votingDict[user]
+		if (abs(guess-result) < abs(closestGuess-result)):
+			closestUser = user
+			closestGuess = guess
+	return [closestUser,closestGuess]
+
+irc = IRC()
+irc.connect(HOST,CHAN,NICK,PASS)
+
+while 1:
+	text = irc.get_text()
+	print text
+	
+	if "PRIVMSG" in text and CHAN in text:
+		sender = text.split("!")[0].split(":")[1]
+		message = text.split(":")[2]
+		#print(sender)
+		#print(message)
+		if sender == 'xandotoaster':
+			if "!" in message:
+				command = message.split("!")[1]
+				if command == "initGuess\r\n":
+					state = 1
+					#print(state)
+					irc.send(CHAN,"Voting has begun! Votes should look like: '!vote 123'")
+					thread.start_new_thread(voteTimer, ())
+					
+				if command == "result\r\n":
+					state = 0
+					result = re.findall(r"[0-9]+",command)[0]
+					print(result)
+					winner = tally(int(result))
+					irc.send(CHAN,"The results are in! The winner is " + winner[0] + " with a guess of " + str(winner[1]) +"!")
+					clearDict()
+					print(votingDict)
+					
+			
+			
+		if "!guess" in message and state != 0:
+			guess = message.split(" ")[1]
+			addGuess(sender,guess)
+			irc.send(CHAN,sender + " has guessed " + guess)
+		if "!guess" in message and state == 0:
+			irc.send(CHAN,"Sorry, " + sender + ", but there is no vote right now")
+			
+	sleep(1.5)
+	
