e896f621d7178178e01ccb7bc44d1c2ae5ac195a
andrewhuntsmith <andrewhuntsmith@gmail.com>
2017-02-02 16:29:25 -0800

    Create blank config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#config.py
+#contains necessary information to connect to twitch chat
+
+HOST = "irc.chat.twitch.tv"
+PORT = 6667
+NICK = "whatever username you have"
+PASS = "oauth: this password is randomly generated, and linked to your username"
+CHAN = "#whatever channel you are adding the bot to"
